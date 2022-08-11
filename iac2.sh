8530fb6a25136869bb8055a2c931ec9b4a31cab4
euraphaeldev <euraphael.dev@gmail.com>
2022-08-11 02:11:34 +0000

    Script de provisionamento Apache v1.0

---
+++ iac2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# UPDATE SERVER
+
+echo 'start update server'
+
+apt-get update
+
+echo 'start upgrade server'
+apt-get upgrade -y
+
+echo 'start install apps'
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo 'go to path /tmp'
+cd /tmp
+
+echo 'downloading git code from https'
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo 'unziping the file downloaded'
+unzip main.zip
+
+echo 'go to path unziped'
+cd linux-site-dio-main
+
+echo 'copy the unziped directory an paste at path /var/www/html/'
+cp -R * /var/www/html/
+
+
